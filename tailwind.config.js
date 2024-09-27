5475bf97826c3f7ff4a0a56e97b03d1d17912092
Jack <jackjonelfiel@gmail.com>
2024-09-27 23:51:34 +0800

    final commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,js}", // Adjust this according to your file structure
+    "./**/*.{html,js}", // Adjust this according to your file structure
   ],
   theme: {
     extend: {},
